79133992367f0a80f2770e40dae2d76f4eabda4d
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-12-20 23:03:08 -0600

    done

---
+++ src/dictionary.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'data.csv': -1,
+  'data2.csv': 1
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const readAllFiles = require('./csv/read');
 const write = require('./csv/write');
+const dictionary = require('./dictionary');
+
+const multiplyByDictionary = (data) => {
+  return data.map((data) => {
+    if (Object.keys(data).length === 0) {
+      return data;
+    } else {
+      data.Amount *= dictionary[data.FileName];
+      return data;
+    }
+  });
+};
 
 const parseData = async () => {
   const dataFolderName = 'data';
+
   const data = await readAllFiles(dataFolderName);
-
   const mergedData = [].concat.apply([], data);
+  const fixNegatives = multiplyByDictionary(mergedData);
 
-  console.log('merged data: ', mergedData);
-
-  write(mergedData);
+  write(fixNegatives);
 };
 
 parseData();
